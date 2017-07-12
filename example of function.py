3bbcc3d4d90c2eb9ffa3c117e6686b79718d457b
miyanda2 <micheal@uplift.ng>
2017-07-12 18:48:18 +0100

    Add files via upload

---
+++ example of function.py
@@ -0,0 +1,5 @@
+def greet(name):
+    """this function greets the person passed in as parameter"""
+    print("hello, " +  name + ". Good morning")
+
+greet('paul')
---
+++ python modules.py
@@ -0,0 +1,7 @@
+def add(a, b):
+   """This program adds two
+   numbers and return the result"""
+a = 1
+b = 2
+result = a + b
+return result
