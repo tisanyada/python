1f2dac56ed818628f46e557918b87a7f2bfd5651
tisanyada <45794283+tisanyada@users.noreply.github.com>
2019-10-11 14:24:43 +0100

    Update output formatting.py

--- output formatting.py
+++ output formatting.py
@@ -3,3 +3,13 @@ print('I love {0} and {1}'.format('bread','butter'))
 
 print('I love {1} and {0}'.format('bread','butter'))
 # Output: I love butter and bread
+
+## #output of a few arithemetic expressions
+x = 3
+y = 4
+
+print('The product of the two numbers is {} \n While the sum is {}'.format(x*y, x+y))
+## using the range method
+for i in range(1,11):
+	for j in range(1,10):
+		print("The mult is {} x {} = {}".format(i,j, j*i))
